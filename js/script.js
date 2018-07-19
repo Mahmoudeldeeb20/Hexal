890bf16f19cd5431fa4a516464679fb8205116a0
Mahmoudeldeeb20 <mahmoud-el-deeb@hotmail.com>
2018-07-19 13:11:57 +0200

    Update 0.12

--- js/script.js
+++ js/script.js
@@ -1,7 +1,10 @@
 $(document).ready(function () {
+        $('#body').show();
         $('.portfolio_slider').slick({
                 dots: true,
                 arrows: false,
                 dotsClass: 'slider_indicators',
         });
+        new WOW().init();
+
 });
